a24e6712743b843d34ee6ec2f255d2b682e8d8da
Максим Наконечный <gembrilus@gmail.com>
2019-09-30 21:03:23 +0300

    Added new tasks on the topic 'Notifications'

---
+++ nit_notification/app/src/main/java/com/example/company/myapplication/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.company.myapplication
+
+import android.app.NotificationChannel
+import android.app.NotificationChannelGroup
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        notify.setOnClickListener {
+            NotificationCompat.Builder(this, "1").apply {
+                setSmallIcon(R.drawable.notification_icon_background)
+                setContentTitle("Title")
+                setContentText("Text")
+            }.build().also { (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager).notify(1, it) }
+        }
+    }
+}
